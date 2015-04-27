20b6a3528f230a1b27fe1a83aceaff67c9dd1404
Serkin Alexander <serkin.alexander@gmail.com>
2015-04-27 15:30:44 +0300

    Remove xml validation tests

--- tests/unit/YMLParser.php
+++ tests/unit/YMLParser.php
@@ -1,35 +1,33 @@
 <?php
 
 use YMLParser\YMLParser;
+use YMLParser\Driver;
 
 class YMLParser_YMLParser extends PHPUnit_Framework_TestCase
 {
     
 
 
-    public function testErrorOnFileDoesntExists()
+    /**
+     * @expectedException \Exception
+     */
+    public function testExceptionOnFileDoesntExists()
     {
         
         $fakefile = dirname(__DIR__) . '/fixtures/non_existing_file.xml';
-        $yml = new YMLParser(new \YMLParser\Driver\XMLReader);
+        $yml = new YMLParser(new Driver\XMLReader);
+        $yml->open($fakefile);
+    }
+    
+
+    public function testOpenExistingFile()
+    {
+
+        $fakefile = dirname(__DIR__) . '/fixtures/valid_xml.xml';
+        $yml = new YMLParser(new Driver\XMLReader);
+
         $result = $yml->open($fakefile);
-
-        $this->assertFalse($result, "File shouldn/t be found");
-        $this->assertTrue($yml->hasError());
-        $this->assertEquals(YMLParser::ERROR_FILE_DOESNT_EXIST, $yml->getErrorCode());
-    }
-
-    public function testErrorOnBrokenFile()
-    {
-    }
-
-
-    public function testValidXML()
-    {
-        $valid_xml_file = dirname(__DIR__) . '/fixtures/valid_xml.xml';
-        $yml = new YMLParser(new \YMLParser\Driver\XMLReader);
-        $result = $yml->open($valid_xml_file);
-
+        
         $this->assertTrue($result);
     }
 }
