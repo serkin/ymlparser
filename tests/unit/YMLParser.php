835f1263963c234cdf9007e075712ca51db2bee8
Serkin Alexander <serkin.alexander@gmail.com>
2015-04-28 15:44:39 +0300

    Move testOpenExistingFile() to driver suit

--- tests/unit/YMLParser.php
+++ tests/unit/YMLParser.php
@@ -5,29 +5,18 @@ use YMLParser\Driver;
 
 class YMLParser_YMLParser extends PHPUnit_Framework_TestCase
 {
-    
-
 
     /**
      * @expectedException \Exception
      */
     public function testExceptionOnFileDoesntExists()
     {
-        
-        $fakefile = dirname(__DIR__) . '/fixtures/non_existing_file.xml';
+
+        $filename = dirname(__DIR__) . '/fixtures/non_existing_file.xml';
         $yml = new YMLParser(new Driver\XMLReader);
-        $yml->open($fakefile);
+        $yml->open($filename);
     }
-    
 
-    public function testOpenExistingFile()
-    {
 
-        $fakefile = dirname(__DIR__) . '/fixtures/valid_xml.xml';
-        $yml = new YMLParser(new Driver\XMLReader);
 
-        $result = $yml->open($fakefile);
-        
-        $this->assertTrue($result);
-    }
 }
